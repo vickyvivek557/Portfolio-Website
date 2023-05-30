bf6ad533e23fc6470c455bca245ddbc50e941430
VIVEK <vickyvivek557@gmail.com>
2023-05-31 02:39:21 +0530

    First Commit

---
+++ script.js
@@ -0,0 +1,34 @@
+function goToAbout() {
+    var elmntToView = document.querySelector(".about");
+    elmntToView.scrollIntoView({
+        behavior: "smooth",
+        block: "end",
+        inline: "nearest",
+    });
+}
+
+function goToProjects() {
+    var elmntToView = document.querySelector(".project");
+    elmntToView.scrollIntoView({
+        behavior: "smooth",
+        block: "start",
+        inline: "nearest",
+    });
+}
+function goToConnect() {
+    var elmntToView = document.querySelector(".connect");
+    elmntToView.scrollIntoView({
+        behavior: "smooth",
+        block: "start",
+        inline: "nearest",
+    });
+}
+
+let aboutBtn = document.querySelector(".about-nav");
+aboutBtn.onclick = goToAbout;
+
+let projectsBtn = document.querySelector(".projects-nav");
+projectsBtn.onclick = goToProjects;
+
+let connectBtn = document.querySelector(".connect-nav");
+connectBtn.onclick = goToConnect;
